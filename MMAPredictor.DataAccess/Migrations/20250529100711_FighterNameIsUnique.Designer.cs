82151f328bc2328c294d728b7047f10d66d087e2
Mahmoud Gatfi <mgatfi@sunlightsolutions.com>
2025-05-29 12:16:46 +0200

    Add migration for making fighter name unique

--- MMAPredictor.DataAccess/Migrations/20250529100711_FighterNameIsUnique.Designer.cs
+++ MMAPredictor.DataAccess/Migrations/20250529100711_FighterNameIsUnique.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MMAPredictor.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MMAPredictor.DataAccess.Migrations
 {
     [DbContext(typeof(MMAPredictorDbContext))]
-    partial class MMAPredictorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250529100711_FighterNameIsUnique")]
+    partial class FighterNameIsUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
---
+++ MMAPredictor.DataAccess/Migrations/20250529100711_FighterNameIsUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MMAPredictor.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FighterNameIsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Fighters_Name",
+                table: "Fighters",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Fighters_Name",
+                table: "Fighters");
+        }
+    }
+}
